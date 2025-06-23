718c064204f1510fc3ce17947cae8237bf97cccc
ruma847 <rumaambarish@gmail.com>
2025-06-23 21:54:18 +0530

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,11 @@
+mangoes1 = 23
+mangoes2 = 76
+mangoes3 = 23
+mangoes4 = 90
+mangoes5 = 37
+
+sum = mangoes1 + mangoes2 + mangoes3 + mangoes4 + mangoes5
+print("the sum of 5 mangoes",sum)
+
+average = sum/5
+print ("find the average",average)
